3c17d6947deefdce87fa5940dd7b945f6293736a
Kaiwen Hu <kaiwen.hu@avanade.com>
2022-10-25 17:27:27 +0900

    changed message from the Home Controller

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure App Service (in the staging slot)!";
+        return "Hello from Azure App Service! Let's start connecting to Azure SQL Server!";
     }
 }
