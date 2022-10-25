35dc6d5483e8b1127c5d6d5d80701ff16c385057
Kaiwen Hu <kaiwen.hu@avanade.com>
2022-10-25 19:37:25 +0900

    added home.html and Description entity

---
+++ src/main/java/com/example/demo/entity/Description.java
@@ -0,0 +1,73 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DESCRIPTIONS")
+public class Description {
+	
+	public Description() {
+		
+	}
+
+	public Description(String caption, String filename, String location) {
+		super();
+		this.caption = caption;
+		this.filename = filename;
+		this.location = location;
+	}
+
+	@Id
+	private Long id;
+
+	@Column
+	private String caption;
+
+	@Column
+	private String filename;
+
+	@Column
+	private String location;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public String getFileName() {
+		return filename;
+	}
+
+	public void setFileName(String filename) {
+		this.filename = filename;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Description [id=" + id + ", caption=" + caption + ", fileName=" + filename + ", location="
+				+ location + "]";
+	}
+	
+}
