b53c3fc4bf2bd04de68664b4ec449e5eb21b7760
Kaiwen Hu <kaiwen.hu@avanade.com>
2022-10-25 20:49:03 +0900

    added posts.js

---
+++ src/main/resources/static/js/posts.js
@@ -0,0 +1,23 @@
+'use strict';
+const HTTP_BAD_REQUEST = 400;
+$('.delete').on('click', function() {
+	let id = this.id;
+	$.ajax({
+		type: "DELETE",
+		url: "../posts/" + id,
+		//data: JSON.stringify(newItem),
+		//dataType: 'json',
+		//contentType: 'application/json'
+	}).done(function (data, textStatus, jqXHR){
+		location.href = "../posts";
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR.status);
+        console.log(textStatus);
+        console.log(errorThrown);
+    });
+});
+
+$('.edit').on('click', function() {
+	let id = this.id;
+	location.href = "../edit-post?id=" + id; 
+});
