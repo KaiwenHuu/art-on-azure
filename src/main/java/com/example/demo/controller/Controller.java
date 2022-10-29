6935a2322abbbd29fc0ed2f0a056637381dad017
Kaiwen Hu <kaiwen.hu@avanade.com>
2022-10-29 12:46:20 +0900

    updated controller

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -47,27 +47,27 @@ public class Controller {
 		return "Hello from Azure App Service in the staging slot! We just implemented Cache Service!";
 	}
 
-	// @GetMapping("/posts")
-	// public ModelAndView getPosts(ModelMap model) {
-	// 	ModelAndView modelAndView = new ModelAndView();
-	// 	List<Description> descriptions = descriptionRepository.findAll();
-
-	// 	model.addAttribute("descriptions", descriptions);
-	// 	modelAndView.setViewName("posts");
-	// 	return modelAndView;
-	// }
-
 	@GetMapping("/posts")
-	public ModelAndView getPostsFromCache(ModelMap model) {
+	public ModelAndView getPosts(ModelMap model) {
 		ModelAndView modelAndView = new ModelAndView();
-		String cacheKey = keyVaultService.getSecret("cacheKey");
-		cacheService = new CacheService(cacheKey);
-		List<Object> descriptions = cacheService.getFromCache("*");
+		List<Description> descriptions = descriptionRepository.findAll();
 
 		model.addAttribute("descriptions", descriptions);
 		modelAndView.setViewName("posts");
 		return modelAndView;
 	}
+
+	// @GetMapping("/posts")
+	// public ModelAndView getPostsFromCache(ModelMap model) {
+	// 	ModelAndView modelAndView = new ModelAndView();
+	// 	String cacheKey = keyVaultService.getSecret("cacheKey");
+	// 	cacheService = new CacheService(cacheKey);
+	// 	List<Object> descriptions = cacheService.getFromCache("*");
+
+	// 	model.addAttribute("descriptions", descriptions);
+	// 	modelAndView.setViewName("posts");
+	// 	return modelAndView;
+	// }
 
 	@RequestMapping("/new-art")
 	public ModelAndView addArtPage() {
